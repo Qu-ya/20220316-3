6aa650210c10979812094c5991a3999aa6a43f88
Qu-ya <liyunliou23@gmail.com>
2022-03-16 17:21:13 +0800

    加入專案檔案。

---
+++ 20220316-3/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*namespace _20220316_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int stars;
+            Console.Write("Stars:");
+            stars = Convert.ToInt32(Console.ReadLine());
+            for (int i = 0; i < stars; i++)
+            {
+                Console.Write("★");
+            }
+            Console.WriteLine("");
+            Console.ReadLine();
+        }
+    }
+}*/
+
+/*namespace _20220316_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+                for (int i = 10; i>0; i-=2 )
+            { 
+                if (i == 3) continue;
+                Console.Write("｛0｝", i);
+            }
+                Console.ReadLine();
+        }
+    }
+}*/
+
+namespace _20220316_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string a = "";
+            string b = "";
+            int x = 0;
+            int y = 0;
+
+            for (int i = 0; i < 60)
+        }
+    }
+}
